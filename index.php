31f07d855a8e388cdcaf6630ab250bfbd8b3b3c5
stefan-urquelle <163069691+stefan-urquelle@users.noreply.github.com>
2024-03-23 12:54:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hello World!";
+echo "Congratulations!"
+echo "Your BallotOnline POC PHP Web application is now running in your own dedicated environment in Microsoft Azure App Service!"
+echo "You are running PHP version 8.2";
